b62bddec5c40a8da37c8ea5399be5d2a3bdd79cf
Lego79 <kentcandoit89@gmail.com>
2022-10-26 20:47:13 +0900

    http requestbody 제이슨 테스트

---
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -0,0 +1,12 @@
+package hello.servlet.basic.response;
+
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "ResponseHeaderServlet", urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+
+    ser
+
+}
